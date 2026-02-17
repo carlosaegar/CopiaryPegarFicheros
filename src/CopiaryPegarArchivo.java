4ddec63e046998ef1192c7e5c2b3c3dbcfe4b8e4
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-02-17 14:41:57 +0100

    Update CopiaryPegarArchivo.java

--- src/CopiaryPegarArchivo.java
+++ src/CopiaryPegarArchivo.java
@@ -1,3 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CopiaryPegarArchivo {
+    public static void main(String[] args) {
+
+        //declaramos un lector y un escritor
+        try (BufferedReader lector = new BufferedReader(new FileReader("origen.txt"));
+             BufferedWriter escritor = new BufferedWriter(new FileWriter("destino.txt"))) {
+
+            String linea;
+            while ((linea = lector.readLine()) != null) {
+                escritor.write(linea);
+                escritor.newLine();
+            }
+
+            System.out.println("archivo copiado");
+
+        } catch (IOException e) {
+            System.out.println("Error : " + e.getMessage());
+
+        }
+    }
+}
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
